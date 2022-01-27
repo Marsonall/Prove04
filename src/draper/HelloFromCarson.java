3c9f41cb5e0e14eda0cb529601b598a00eacc3d1
Carson Draper <dra19001@byui.edu>
2022-01-26 21:12:00 -0700

    Initial commit

---
+++ src/draper/HelloFromCarson.java
@@ -0,0 +1,13 @@
+package draper;
+
+public class HelloFromCarson {
+
+    public void sayHello() {
+        System.out.println("Hello Carson Draper");
+    }
+
+    public static void main(String[] args) {
+        HelloFromCarson hello = new HelloFromCarson();
+        hello.sayHello();
+    }
+}
